e8b12716bc96e160e075c34df5349512eee65842
Daniel Grigg <daniel.grigg@covata.com>
2014-01-13 15:00:28 +1100

    Initial Commit

---
+++ cityfs/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  cityfs
+//
+//  Created by Daniel Grigg on 13/01/2014.
+//  Copyright (c) 2014 Daniel Grigg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
